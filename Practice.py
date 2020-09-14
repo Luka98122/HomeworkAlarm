7cdb00a7daf83e83b821cc68b12ad7dd19b45197
Luka98122 <luka98122@gmail.com>
2020-09-14 19:28:37 +0200

    I handed file not found

--- Practice.py
+++ Practice.py
@@ -4,10 +4,14 @@ def myFunc(t):
         print("t is 1")
     
     print("This is still in the function")
-with open ("TEST.txt", 'r') as m:
-    e =m.readlines()
-    print(e)
-    print("HERE")
+
+try:
+    with open ("TEST.txt", 'r') as m:
+        e = m.readlines()
+        print(e)
+        print("HERE")
+except:
+    print('File not found')
 
 gold = 100
 f = 2
@@ -31,3 +35,4 @@ with open("TEST.txt", 'wt') as t:
     t.write(str(gold)+'\n')
     for i in range(10):
         t.write(str(i) + "\n")
+

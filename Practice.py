da1e980f21a2769359af2b6c79e49a67e62ede18
Luka98122 <luka98122@gmail.com>
2020-09-14 20:18:14 +0200

    Learned the initialize pattern

--- Practice.py
+++ Practice.py
@@ -1,38 +1,26 @@
-def myFunc(t):
-    print("myFunc called with " + str(t))
-    if t == 1:
-        print("t is 1")
-    
-    print("This is still in the function")
+print('Hello world')
+gold = 100
 
 try:
     with open ("TEST.txt", 'r') as m:
         e = m.readlines()
         print(e)
-        print("HERE")
-except:
+        print
+        
+        gold = int(e[0]) * 2
+        
+except FileNotFoundError:
     print('File not found')
 
-gold = 100
-f = 2
-print("Hello world")
-print(f)
+print('File read')
 
-if f==1:
-    print("f is one")
-else:
-    print("f is not one")
-
-print("We are out of the if", end='')
-myFunc(f)
-
-for i in range(10):
-    for k in range(i):
-        print("o",end='')
-    print("")
 
 with open("TEST.txt", 'wt') as t:
+    
     t.write(str(gold)+'\n')
+   
+
     for i in range(10):
         t.write(str(i) + "\n")
 
+print(gold)
